e43430194e1ba957173a9274e5869eea3e69417a
k.nitta <k.nitta>
2017-03-16 15:30:31 +0900

    構成変更

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -2,15 +2,14 @@
 from __future__ import print_function, unicode_literals
 
 from flask import Flask, render_template, request
-
-app = Flask(__name__)
-app.config.update({'DEBUG': True })
+from flaskapp import app
 
 @app.route('/')
-def random_create():
+def trump_operation():
+    '''
+    formで入力されたactionの値{random, sort}によって,
+    トランプの操作を行う
+    '''
     _action = request.args.get('action')
 
     return render_template('trump.html', result=_action)
-
-if __name__ == '__main__':
-    app.run()
